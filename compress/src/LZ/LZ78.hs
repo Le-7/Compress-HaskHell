cc5382976ff6c5d90f4bc6237c0e7dfbfc1ded1b
Le_sept_ou_le_siete <76405539+Le-7@users.noreply.github.com>
2024-03-19 20:57:18 +0100

    Refactoring of Anissa's wonderful code

--- compress/src/LZ/LZ78.hs
+++ compress/src/LZ/LZ78.hs
@@ -1,7 +1,7 @@
 module LZ.LZ78 (compress, uncompress) where
 
-import LZ.Dictionaries (empty, zeroAsChar)
-import Data.List (elemIndex, isPrefixOf)
+import LZ.Dictionaries (empty)
+import Data.List (elemIndex)
 import Data.Maybe (fromMaybe)
 
 type Dictionary = [String]
@@ -20,19 +20,6 @@ compress2helper dict (x:xs) prefix =
   case elemIndex (prefix ++ [x]) dict of
     Just idx -> compress2helper dict xs (prefix ++ [x])
     Nothing  -> (fromMaybe 0 (elemIndex prefix dict), x) : compress2helper (dict ++ [prefix ++ [x]]) xs []
-
-
-
--- Find the longest prefix of the dictionary that matches the beginning of the string
-findLongestPrefix :: Dictionary -> String -> (String, String)
-findLongestPrefix dict str = go dict "" str
-  where
-    go _ prefix [] = (prefix, "")
-    go [] prefix rest = (prefix, rest)
-    go (entry:entries) prefix rest =
-      if entry `isPrefixOf` rest
-        then go entries entry (drop (length entry) rest)
-        else go entries prefix rest
 
 -- | Uncompress a list of LZ78-encoded tuples
 uncompress :: [(Int, Char)] -> Maybe String
