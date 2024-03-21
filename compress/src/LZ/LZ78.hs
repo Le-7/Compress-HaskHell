d19b809277b17ddaef4e642083b94eb2434fab06
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2024-03-21 20:34:52 +0100

    Optimisation code pour respect consignes

--- compress/src/LZ/LZ78.hs
+++ compress/src/LZ/LZ78.hs
@@ -12,20 +12,20 @@ compress = compress' empty
 
 compress' :: Dictionary -> String -> [(Int, Char)]
 compress' _ [] = []
-compress' dict str = compress2helper dict str []
+compress' dict str = compressHelper dict str []
 
-compress2helper :: Dictionary -> String -> String -> [(Int, Char)]
-compress2helper _ [] _ = []
+compressHelper :: Dictionary -> String -> String -> [(Int, Char)]
+compressHelper _ [] _ = []
 
-compress2helper dict (x:[]) prefix =
+compressHelper dict (x:[]) prefix =
   case elemIndex (prefix ++ [x]) dict of
     Just idx -> [(fromMaybe 0 (elemIndex prefix dict), x)]
     Nothing  -> [(fromMaybe 0 (elemIndex prefix dict), x)]
 
-compress2helper dict (x:xs) prefix =
+compressHelper dict (x:xs) prefix =
   case elemIndex (prefix ++ [x]) dict of
-    Just idx -> compress2helper dict xs (prefix ++ [x])
-    Nothing  -> (fromMaybe 0 (elemIndex prefix dict), x) : compress2helper (dict ++ [prefix ++ [x]]) xs []
+    Just idx -> compressHelper dict xs (prefix ++ [x])
+    Nothing  -> (fromMaybe 0 (elemIndex prefix dict), x) : compressHelper (dict ++ [prefix ++ [x]]) xs []
 
 -- | Uncompress a list of LZ78-encoded tuples
 uncompress :: [(Int, Char)] -> Maybe String
@@ -35,15 +35,13 @@ uncompress = fmap concat . uncompress' empty
 uncompress' :: Dictionary -> [(Int, Char)] -> Maybe [String]
 uncompress' _ [] = Just []
 uncompress' dict ((index, char):rest) = do
-  prefix <- dict `atIndex` index
-  let newEntry = prefix ++ [char]
-  let newDict = dict ++ [newEntry]
-  suffix <- uncompress' newDict rest
-  return (newEntry : suffix)
+  prefix <- atIndex dict index
+  suffix <- uncompress' (dict ++ [prefix ++ [char]]) rest
+  return ([prefix ++ [char]] ++ suffix)
 
 -- Get the element at a specific index in a list
 atIndex :: [a] -> Int -> Maybe a
 atIndex [] _ = Nothing
 atIndex (x:xs) i
   | i == 0    = Just x
-  | otherwise = xs `atIndex` (i - 1)
+  | otherwise = atIndex xs (i - 1)
