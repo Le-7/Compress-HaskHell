686b531486beca0dc46abab1d7f60ee375e52d77
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2024-03-22 20:41:14 +0100

    Plus de variables

--- compress/src/LZ/LZW.hs
+++ compress/src/LZ/LZW.hs
@@ -1,33 +1,32 @@
 module LZ.LZW (compress, uncompress) where
 
 import Data.List (find)
-import Data.Char(ord)
+import Data.Char (ord)
 
 -- Define the initial dictionary for LZW
 initialDictionary :: [(String, Int)]
 initialDictionary = zip (map (:[]) ['\0'..'\255']) [0..255]
 
--- Function to compress a string using LZW
+-- | LZW compress method
 compress :: String -> [Int]
 compress = compress' initialDictionary ""
 
 -- Helper function for compression
 compress' :: [(String, Int)] -> String -> String -> [Int]
 compress' _ _ [] = []
--- End of input string, output the code for the currentString and stop
 compress' dict currentString [x] = case find (\(s, _) -> s == currentString ++ [x]) dict of
     Just entry -> [snd entry]
     Nothing -> case find (\(s, _) -> s == currentString) dict of
         Just (_, code) -> [code] ++ [ord x]
         Nothing -> error ("Dictionary entry not found during compression: " ++ show currentString ++ " ++ [" ++ show x ++ "]")
--- Process the string
 compress' dict currentString (x:xs) = case find (\(s, _) -> s == currentString ++ [x]) dict of
     Just entry -> compress' dict (currentString ++ [x]) xs
     Nothing -> case find (\(s, _) -> s == currentString) dict of
         Just (_, code) -> code : compress' ((currentString ++ [x], length dict) : dict) [x] xs
         Nothing -> error ("Dictionary entry not found during compression: " ++ show currentString ++ " ++ [" ++ show x ++ "]")
-        
--- Function to uncompress a list of integers using LZW
+
+-- | LZW uncompress method
+-- If input cannot be uncompressed, returns `Nothing`
 uncompress :: [Int] -> Maybe String
 uncompress compressed = Just (uncompress' compressed initialDictionary)
 
@@ -41,14 +40,15 @@ uncompress' (x:y:xs) dict = case find (\(_, code) -> code == x) dict of
     Just (entry, _) ->
         case find (\(_, code) -> code == y) dict of
             Just (nextEntry, _) ->
-                entry ++ uncompress' (y:xs) (updateDictionary (entry ++ [head nextEntry]) dict)
+                appendEntry entry (uncompress' (y:xs) (updateDictionary (entry ++ [head nextEntry]) dict))
             Nothing ->
-                let newEntry = entry ++ [head entry] -- Create a new entry by appending the first character of the current entry
-                in entry ++ uncompress' (y:xs) ((newEntry, length dict) : dict)
+                appendEntry entry (uncompress' (y:xs) ((entry ++ [head entry], length dict) : dict))
     Nothing -> error ("Dictionary entry not found during decompression: " ++ show x)
-
 
 -- Update dictionary with new entry
 updateDictionary :: String -> [(String, Int)] -> [(String, Int)]
 updateDictionary newEntry dict = (newEntry, length dict) : dict
 
+-- Append entry to result
+appendEntry :: String -> String -> String
+appendEntry entry rest = entry ++ rest
