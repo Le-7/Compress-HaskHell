8d85ddd579ee726bd6b0aea4bb0e8529498a6769
Le_sept_ou_le_siete <76405539+Le-7@users.noreply.github.com>
2024-03-22 20:07:07 +0100

    Update ShannonFano.hs

--- compress/src/Statistic/ShannonFano.hs
+++ compress/src/Statistic/ShannonFano.hs
@@ -1,36 +1,31 @@
-{- |
-  Module : Statistic.ShannonFano
-  Description : A module containing specifics for the Shannon-Fano compression method
-  Maintainer : ???
--}
--- Importing necessary modules
+{-# LANGUAGE TupleSections #-}
+
 module Statistic.ShannonFano (tree) where
 
 import Statistic.EncodingTree
-import Data.List (group, sort, sortOn)
-
--- Calculate symbol frequencies
-frequency :: Ord a => [a] -> [(a, Int)]
-frequency = map (\x -> (head x, length x)) . group . sort
-
--- Sort symbols based on frequencies
-sortSymbols :: Ord a => [a] -> [a]
-sortSymbols = map fst . sortOn snd . frequency
+import Statistic.Source (orderedCounts)
 
 -- Recursive function to generate Shannon-Fano tree
-shannonFanoTree :: Ord a => [a] -> Maybe (EncodingTree a)
-shannonFanoTree []  = Just (EncodingNode 0 (EncodingLeaf 0 undefined) (EncodingLeaf 0 undefined))
-shannonFanoTree [x] = Just (EncodingLeaf 1 x)
-shannonFanoTree symbols =
-  let mid = length symbols `div` 2
-      leftSymbols = take mid symbols
-      rightSymbols = drop mid symbols
-      leftTree = shannonFanoTree leftSymbols
-      rightTree = shannonFanoTree rightSymbols
-  in case (leftTree, rightTree) of
-    (Just lt, Just rt) -> Just (EncodingNode (count lt + count rt) lt rt)
-    _                  -> Nothing
+shannonFanoTree :: Ord a => [(a, Int)] -> Maybe (EncodingTree a)
+shannonFanoTree [] = Just (EncodingLeaf 0 undefined)
+shannonFanoTree [(x, _)] = Just (EncodingLeaf 1 x)
+shannonFanoTree xs =
+  let total = sum (map snd xs)
+      half = total `div` 2
+      (left, right) = splitAtHalf half xs
+  in case (shannonFanoTree left, shannonFanoTree right) of
+       (Just lt, Just rt) -> Just (EncodingNode total lt rt)
+       _                  -> Nothing
+
+splitAtHalf :: Int -> [(a, Int)] -> ([(a, Int)], [(a, Int)])
+splitAtHalf _ [] = ([], [])
+splitAtHalf n ((x, cnt):rest)
+  | n' < 0 = ([], (x, cnt):rest)
+  | otherwise = ((x, cnt):left, right)
+  where
+    n' = n - cnt
+    (left, right) = splitAtHalf n' rest
 
 -- Updated tree function using Shannon-Fano tree generation
 tree :: Ord a => [a] -> Maybe (EncodingTree a)
-tree = shannonFanoTree . sortSymbols
+tree symbols = shannonFanoTree (orderedCounts symbols)
