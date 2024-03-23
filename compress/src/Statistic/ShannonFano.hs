0616e6b2d76390a05426faa089302bb0b66b15e6
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2024-03-23 13:06:59 +0100

    Respect des contraintes

--- compress/src/Statistic/ShannonFano.hs
+++ compress/src/Statistic/ShannonFano.hs
@@ -1,31 +1,26 @@
-{-# LANGUAGE TupleSections #-}
-
 module Statistic.ShannonFano (tree) where
 
 import Statistic.EncodingTree
 import Statistic.Source (orderedCounts)
 
--- Recursive function to generate Shannon-Fano tree
-shannonFanoTree :: Ord a => [(a, Int)] -> Maybe (EncodingTree a)
-shannonFanoTree [] = Just (EncodingLeaf 0 undefined)
-shannonFanoTree [(x, _)] = Just (EncodingLeaf 1 x)
-shannonFanoTree xs =
-  let total = sum (map snd xs)
-      half = total `div` 2
-      (left, right) = splitAtHalf half xs
-  in case (shannonFanoTree left, shannonFanoTree right) of
-       (Just lt, Just rt) -> Just (EncodingNode total lt rt)
-       _                  -> Nothing
+-- | Split the distribution into two parts
+splitDistribution :: [(a, Int)] -> ([(a, Int)], [(a, Int)])
+splitDistribution dist = splitAt (findSplitPoint dist 0 (sum (map snd dist))) dist
 
-splitAtHalf :: Int -> [(a, Int)] -> ([(a, Int)], [(a, Int)])
-splitAtHalf _ [] = ([], [])
-splitAtHalf n ((x, cnt):rest)
-  | n' < 0 = ([], (x, cnt):rest)
-  | otherwise = ((x, cnt):left, right)
-  where
-    n' = n - cnt
-    (left, right) = splitAtHalf n' rest
+-- | Find the split point in the distribution
+findSplitPoint :: [(a, Int)] -> Int -> Int -> Int
+findSplitPoint [] _ _ = 0
+findSplitPoint ((_, count) : rest) acc total
+  | acc * 2 >= total || null rest = 0
+  | otherwise = 1 + findSplitPoint rest (acc + count) total
 
--- Updated tree function using Shannon-Fano tree generation
+-- | Build the encoding tree
+buildTree :: [(a, Int)] -> EncodingTree a
+buildTree [(symbol, count)] = EncodingLeaf count symbol
+buildTree distribution =
+  case splitDistribution distribution of
+    (left, right) -> EncodingNode (sum (map snd distribution)) (buildTree left) (buildTree right)
+
+-- | Build the encoding tree using Shannon-Fano algorithm
 tree :: Ord a => [a] -> Maybe (EncodingTree a)
-tree symbols = shannonFanoTree (orderedCounts symbols)
+tree symbols = Just (buildTree (orderedCounts symbols))
