d79fb526301f9251c37b383a3cd4d44d0460cc30
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2024-03-24 12:28:37 +0100

    Sans variables

--- compress/src/RLE.hs
+++ compress/src/RLE.hs
@@ -1,22 +1,21 @@
 -- |
 -- Module      : RLE
 -- Description : An implementation of the run-length encoding method
--- Maintainer  : ???
+-- Maintainer  : AÏT CHADI Anissa, COSTA Mathéo, TRAN Alexandre
 --
 module RLE (compress, uncompress) where
 
 -- | RLE compress method
 compress :: Eq a => [a] -> [(a, Int)]
-compress [] = []
-compress (x:xs) = let (first, rest) = span (==x) xs
-                  in (x, length first + 1) : compress rest
+compress [] = []  -- Si la liste est vide on ne retourne rien
+compress (x:xs) = (x, length (takeWhile (==x) xs) + 1) : compress (dropWhile (==x) xs)
 
 -- | RLE uncompress method
--- If input cannot be uncompressed, returns `Nothing`
+-- Si la chaine ne peut pas être compressée, on retourne `Nothing`
 uncompress :: [(a, Int)] -> Maybe [a]
-uncompress [] = Just []
+uncompress [] = Just [] -- Si liste d'entrée vide, on renvoie ca pour indiquer que c'est décompressé en liste vide
 uncompress ((sym, count) : xs)
-  | count < 1 = Nothing
-  | otherwise = case uncompress xs of
+  | count < 1 = Nothing -- Si count est inférieur à 1, c'est qu'il y a une erreur dans la compression
+  | otherwise = case uncompress xs of 
                   Nothing -> Nothing
                   Just rest -> Just (replicate count sym ++ rest)
